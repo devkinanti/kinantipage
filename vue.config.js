79ac6ba3c383c5f224531673c51d3a4cfab541cc
unodclms <unodclms.id@gmail.com>
2022-08-23 08:19:47 +0700

    disableHostCheck: true

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,6 @@ module.exports = {
   ],
 
   devServer: {
-    allowedHosts: [
-      'pedulicovid19-pemasyarakatan.com', // the domain name address allowed to access, that is, the address of peanut shell intranet penetration
-      '*pedulicovid19-pemasyarakatan.com'   // . Is a wildcard of the secondary domain name   
-    ],
+    disableHostCheck: true
   },
 }
