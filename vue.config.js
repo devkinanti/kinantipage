6c1296c35c6d790f476558069242bcf0ca2668fc
unodclms <unodclms.id@gmail.com>
2022-08-23 07:58:05 +0700

    allow dev server

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  devServer: {
+    allowedHosts: [
+      'pedulicovid19-pemasyarakatan.com', // the domain name address allowed to access, that is, the address of peanut shell intranet penetration
+      '*pedulicovid19-pemasyarakatan.com'   // . Is a wildcard of the secondary domain name   
+    ],
+  },
 }
